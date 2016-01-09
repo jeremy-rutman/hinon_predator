81afabc0b2956b5681e3ab70d1d61bb86b7354a0
jeremy rutman <jeremy.rutman@gmail.com>
2016-01-09 19:44:41 +0000

    34

--- movietime.py
+++ movietime.py
@@ -246,25 +246,25 @@ def masterloop():
 		if 'movie1' in read_string:
                     print('starting movie1')
 		    GPIO.output(pin_start_movie1,True)
-                    a= pexpect.spawn("/usr/bin/omxplayer -o hdmi 1.mp4")
+                    a= pexpect.spawn("sudo /usr/bin/omxplayer -o hdmi 1.mp4")
 		    time.sleep(minpause)		   
 		    GPIO.output(pin_start_movie1,False)
 		if 'movie2' in read_string:
                     print('starting movie2')
 		    GPIO.output(pin_start_movie2,True)
-                    a= pexpect.spawn("/usr/bin/omxplayer -o hdmi 2.mp4")
+                    a= pexpect.spawn("sudo /usr/bin/omxplayer -o hdmi 2.mp4")
 		    time.sleep(minpause)		   
 		    GPIO.output(pin_start_movie2,False)
 		if 'movie3' in read_string:
                     print('starting movie3')
 		    GPIO.output(pin_start_movie3,True)
-                    a= pexpect.spawn("/usr/bin/omxplayer -o hdmi 3.mp4")
+                    a= pexpect.spawn("sudo /usr/bin/omxplayer -o hdmi 3.mp4")
 		    time.sleep(minpause)		   
 		    GPIO.output(pin_start_movie3,False)
 		if 'movie4' in read_string:
                     print('starting movie4')
 		    GPIO.output(pin_start_movie4,True)
-                    a= pexpect.spawn("/usr/bin/omxplayer -o hdmi 4.mp4")
+                    a= pexpect.spawn("sudo /usr/bin/omxplayer -o hdmi 4.mp4")
 		    time.sleep(minpause)		   
 		    GPIO.output(pin_start_movie4,False)
 		if 'zeroing' in read_string:
